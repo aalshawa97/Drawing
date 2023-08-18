9a9110cbd6fb5e6016a3100a0bce550a7164710b
abdullah97 <abdullahalshawa@gmail.com>
2023-08-18 08:47:25 -0700

    Draw square and triangle

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+#Abdullah Mutaz Alshawa
+#8/18/23
+
 import turtle
 
-Abdullah = turtle.Turtle()
+#Set up the screen
+screen = turtle.Screen()
+screen.title("Turtle Graphical User Interface Application")
+screen.bgcolor("white")
 
-Abdullah.speed(1)
+#Create a turtle
+t = turtle.Turtle()
+#Set the turtle's speed
+t.speed(1)
 
-Abdullah.forward(100)
+#Draw a square
+for _ in range(4):
+    #Move the turtle forward by 100 units
+    t.forward(100)
+    #Turn the turtle 90 degrees to the right
+    t.right(90)
 
-Abdullah.left(45)
-
-Abdullah.forward(100)
-
-turtle.done()
+#Draw a triangle
+for _ in range(3):
+    t.forward(100)
+    t.left(120)
+#Close the turtle graphics window when clicked
+screen.exitonclick()
